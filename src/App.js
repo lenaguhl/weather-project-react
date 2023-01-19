0427c4c3c5f15f2f25b682ca835b99242a4a1a3e
lenaguhl <98180093+lenaguhl@users.noreply.github.com>
2023-01-19 16:25:19 +0100

    include weather icon

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export default function App() {
     <div className="App">
       <div className="container">
       <h1>Weather App</h1>
-      <Weather />
+      <Weather defaultCity="New York" />
       <footer>
         This project was coded by Lena Guhl and is 
         <a 
--- src/Weather.js
+++ src/Weather.js
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
-import FormattedDate from "./FormattedDate"
+import WeatherInfo from "./WeatherInfo";
 import axios from "axios";
 import "./Weather.css";
 
 export default function Weather(props) {
     const [weatherData, setWeatherData] = useState({ready: false});
+    const [city, setCity] = useState(props.defaultCity);
     function handleResponse(response) {
         setWeatherData({
             ready: true,
@@ -12,17 +13,31 @@ export default function Weather(props) {
             humidity: response.data.main.humidity,
             date: new Date(response.data.dt * 1000),
             description: response.data.weather[0].description,
-            iconUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+            iconUrl: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
             wind: response.data.wind.speed,
             city: response.data.name
         });
+    }
 
+    function search() {
+      const apiKey = "987c9ba48a6ccc45c1fbcb73388f4b27";
+        let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+        axios.get(apiUrl).then(handleResponse);
+    }
+
+    function handleSubmit(event) {
+      event.preventDefault();
+      search();
+    }
+
+    function handleCityChange(event) {
+      setCity(event.target.value);
     }
 
     if (weatherData.ready) {
         return (
             <div className="Weather">
-              <form>
+              <form onSubmit={handleSubmit}>
                 <div className="row">
                   <div className="col-9">
                     <input
@@ -30,6 +45,7 @@ export default function Weather(props) {
                       placeholder="Enter a city.."
                       className="form-control"
                       autoFocus="on"
+                      onChange={handleCityChange}
                     />
                   </div>
                   <div className="col-3">
@@ -41,42 +57,13 @@ export default function Weather(props) {
                   </div>
                   </div>
               </form>
-              <h1>{weatherData.city}</h1>
-              <ul>
-                  <li>
-                    <FormattedDate date={weatherData.date} />
-                    </li>
-                  <li className="text-capitalize">{weatherData.description}</li>
-              </ul>
-              <div className="row mt-3">
-              <div className="col-6">
-              <div className="clearfix">
-                  <img 
-                  src={weatherData.icon} 
-                  alt={weatherData.description}
-                  className="float-left" 
-                  />
-                  <div className="float-left">
-                  <span className="temperature">{Math.round(weatherData.temperature)}</span>
-                  <span className="unit">°C</span>
-                  </div>
-              </div>
-              </div>
-              <div className="col-6">
-              <ul>
-                  <li>Precipitation: 15%</li>
-                  <li>Humidity: {weatherData.humidity} %</li>
-                  <li>Wind: {weatherData.wind} km/h</li>
-              </ul>    
-              </div>
-              </div>
+              <WeatherInfo data={weatherData}/>
+
+              
             </div>
           );
     } else {
-        const apiKey = "987c9ba48a6ccc45c1fbcb73388f4b27";
-        let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?&appid=${apiKey}&units=metric`;
-        axios.get(apiUrl).then(handleResponse);
-
-        return "Loading"
+      search();
+        return "Loading";
     }
 }
