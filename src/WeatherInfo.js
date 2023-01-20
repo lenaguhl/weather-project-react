776b15f81a679d98cd175b3380f36ca3ebfc5075
lenaguhl <98180093+lenaguhl@users.noreply.github.com>
2023-01-20 09:53:20 +0100

    update css

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -5,34 +5,34 @@ import WeatherTemperature from "./WeatherTemperature"
 export default function WeatherInfo(props) {
     return (
     <div className="WeatherInfo">
-        <h1>{props.data.city}</h1>
-    <ul>
-        <li>
-          <FormattedDate date={props.data.date} />
-          </li>
-        <li className="text-capitalize">{props.data.description}</li>
-    </ul>
-    <div className="row mt-3">
-    <div className="col-6">
-    <div className="clearfix">
-        <img 
-        src={props.data.icon} 
-        alt={props.data.description}
-        className="float-left" 
-        />
-
-        <div className="float-left">
-            <WeatherTemperature celsius={props.data.temperature} />
+        
+      <h1>{props.data.city}</h1>
+ 
+      <div className="row">
+        <div className="col-3">
+              <img 
+               src={props.data.icon} 
+               alt={props.data.description}
+              />
         </div>
-    </div>
-    </div>
-    <div className="col-6">
-    <ul>
-        <li>Humidity: {props.data.humidity} %</li>
-        <li>Wind: {props.data.wind} km/h</li>
-    </ul>    
-    </div>
-    </div>
-    </div>
+        
+        <div className="col-3">
+              <WeatherTemperature celsius={props.data.temperature} />
+        </div>
+    
+        <div className="col-6">
+          <ul>
+            <li>Humidity: {props.data.humidity} %
+            </li>
+            <li>Wind: {props.data.wind} km/h
+            </li>
+          </ul>    
+        </div>
+        <div className="text-capitalize">{props.data.description}
+        </div>
+        <div className="date"><FormattedDate date={props.data.date} />
+        </div>
+      </div>
+    </div>  
     );
 }
